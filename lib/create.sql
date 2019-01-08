9937532fc4015240b637bd58c1d73396acd011ef
Roberto Samia <bojosteph@gmail.com>
2019-01-08 19:30:23 +0000

    Automatically backed up by Learn

--- lib/create.sql
+++ lib/create.sql
@@ -1,13 +1,14 @@
-def creat
- CREATE TABLE bears (
- id Integer PRIMARY KEY,
- name TEXT,
+def creating_bears_table
+
+" CREATE TABLE bears ("
+"id Integer PRIMARY KEY,"
+" name TEXT,"
  age INTEGER,
  gender TEXT,
  color TEXT,
  temperament TEXT,
  alive BOOLEAN
  
-  ); "
- 
+  ); 
+ end
  
