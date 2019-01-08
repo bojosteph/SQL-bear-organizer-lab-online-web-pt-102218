ea179aa84ffb8e839cb58615cb1d1bc94d92f4b2
Roberto Samia <bojosteph@gmail.com>
2019-01-08 19:38:49 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,1 +1,2 @@
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 9, "M", "brown", "always hungry", 1)
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES 
